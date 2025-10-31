20a940bddad27105462b343072cac518f83a6f30
천자민 <40034379+storyrago@users.noreply.github.com>
2025-10-31 22:28:18 +0900

    Update 체육복.java

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,7 +1,7 @@
 class Solution {
     public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int [] clo = new int[n+2];
+        int [] clo = new int[n+2]; // ArrayIndexOutOfBoundsException 방지
         for(int i : reserve) clo[i]++;
         for(int i : lost) clo[i]--;
         //1이면 여분있는 애들, 0이면 자기꺼 가지고 있는 애들, -1이면 없는 애들
