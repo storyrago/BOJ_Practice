0a52c5a1f6503d244daa20a80f08432992131488
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2025-11-18 15:22:50 +0900

    [level 2] Title: 피보나치 수, Time: 2.90 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,12 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[]fibo = new int[100001];
-        fibo[0] = 0; fibo[1] = 1;
+        int[]dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
         for(int i = 2;i<=n;i++){
-            fibo[i] = fibo[i-1] % 1234567 + fibo[i-2] % 1234567;
+            dp[i] = dp[i-1]% 1234567 + dp[i-2] % 1234567;
         }
-        answer = fibo[n] % 1234567;
+        answer = dp[n] % 1234567; 
         return answer;
     }
 }
