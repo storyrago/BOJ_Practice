151631472dd31aceaab455d32b2a5546e170b18a
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2025-11-19 17:44:19 +0900

    [level 2] Title: 구명보트, Time: 11.73 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,22 +2,23 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         Arrays.sort(people);
-        int len = people.length;
-        int minW = 0;
-        int maxW = len-1;
-        while(minW <= maxW){
-            if(people[minW] + people[maxW] <= limit){
-                answer++;
-                minW++;
-                maxW--;
+        int bottom = 0;
+        int top = people.length - 1;
+        int boat = 0;
+        while(bottom <= top){
+            int sum = people[bottom] + people[top];
+            if(sum > limit){
+                top--;
+                boat++;
             }
             else{
-                answer++;
-                maxW--;
+                bottom++;
+                top--;
+                boat++;
             }
         }
-        return answer;
+
+        return boat;
     }
 }
