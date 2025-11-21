3d7688a5aab2b781f0a8f5952ff6a6baa1c48191
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2025-11-21 16:15:54 +0900

    [level 2] Title: 타겟 넘버, Time: 4.70 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,10 +1,11 @@
 class Solution {
     static int CNT;
     static void dfs(int[]numbers,int target,int start, int prev){
-        if(start == numbers.length) {
-            if(prev == target) CNT++;
+        if(start == numbers.length) { // start 가 끝까지왔고
+            if(prev == target) CNT++; // prev 가 target 이면 CNT 값 증가
             return;
         }
+        // index 값 넘겨주고, 지난 값에 numbers[인덱스] 인거 같은데 왜 안되냐고
         dfs(numbers,target,start + 1, prev + numbers[start]);
         dfs(numbers,target,start + 1, prev - numbers[start]);
     }
