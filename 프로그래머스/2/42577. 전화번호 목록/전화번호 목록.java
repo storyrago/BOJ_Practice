c111ade8d742c0e5878791eec2d0012bdf032f21
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2025-11-28 12:23:09 +0900

    [level 2] Title: 전화번호 목록, Time: 275.38 ms, Memory: 255 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,18 +4,17 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         HashMap<String,Integer> map = new HashMap<>();
-        for(int i = 0;i< phone_book.length;i++){
-            map.put(phone_book[i], 1);
+        for(String str : phone_book){
+            map.put(str,1);
         }
         for(String str : phone_book){
-            for(int i = 1;i<str.length();i++){
+            for(int i = 0;i<str.length();i++){
                 String temp = str.substring(0,i);
                 if(map.containsKey(temp)){
                     answer = false;
-                    break;
+                    return answer;
                 }
             }
-            if(!answer) break;
         }
         return answer;
     }
