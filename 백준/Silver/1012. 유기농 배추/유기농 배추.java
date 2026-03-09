472b513ba90471b8918c2224d7e4ed92544589f5
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2026-03-09 23:13:37 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 17232 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,66 +1,58 @@
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
+import java.io.IOException;
 
 public class Main {
     static int M;
     static int N;
     static int K;
-
-    public static void bfs(int[][] graph,boolean[][]checked,int starty,int startx){
-        Queue<int []> q = new LinkedList<>();
-        checked[starty][startx] = true;
-        q.add(new int[]{starty,startx});
+    public static void bfs(int[][]board,boolean[][]checked, int startY, int startX){
         int[]dy = {-1,0,1,0};
         int[]dx = {0,1,0,-1};
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{startY, startX});
+        checked[startY][startX] = true;
         while(!q.isEmpty()){
             int[]temp = q.poll();
-            int y = temp[0];
-            int x = temp[1];
+            int ty = temp[0];
+            int tx = temp[1];
             for(int i = 0;i<4;i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if(ny >= 0 && ny<N && nx >=0 && nx <M && !checked[ny][nx] && graph[ny][nx] == 1){
-                    q.add(new int[]{ny,nx});
+                int ny = ty + dy[i];
+                int nx = tx + dx[i];
+                if(ny >= 0 && ny < N && nx >= 0 && nx < M && !checked[ny][nx] && board[ny][nx] != 0){
                     checked[ny][nx] = true;
+                    bfs(board,checked,ny,nx);
                 }
             }
         }
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-
         for(int i = 0;i<T;i++){
             st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken()); //가로
-            N = Integer.parseInt(st.nextToken()); //세로
-            K = Integer.parseInt(st.nextToken()); //배추 개수
-            int[][]graph = new int[N][M];
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            int[][]board = new int[N][M];
             boolean[][]checked = new boolean[N][M];
             for(int j = 0;j<K;j++){
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                graph[b][a] = 1;
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                board[Y][X] = 1;
             }
-            int ans = 0;
+            int cnt = 0;
             for(int j = 0;j<N;j++){
                 for(int k = 0;k<M;k++){
-                    if(!checked[j][k] && graph[j][k] == 1){
-                        ans++;
-                        bfs(graph,checked,j,k);
+                    if(!checked[j][k] && board[j][k] == 1){
+                        cnt++;
+                        bfs(board,checked,j,k);
                     }
                 }
             }
-            System.out.println(ans);
+            System.out.println(cnt);
         }
-
     }
 }
-/*
-
-5
-3 1 4 3 2
-* */
