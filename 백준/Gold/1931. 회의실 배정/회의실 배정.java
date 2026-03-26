7090d45c4c4f70243641f2aed75341b3ce88b586
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2026-03-26 15:56:49 +0900

    [Gold V] Title: 회의실 배정, Time: 544 ms, Memory: 45548 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,37 +1,39 @@
 import java.util.*;
 import java.io.*;
+import java.io.IOException;
 
 public class Main {
+    static class Period{
+        int start;
+        int end;
+        public Period(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        ArrayList<int []>schedul = new ArrayList<>();
         int n = Integer.parseInt(br.readLine());
-        for(int i = 0;i<n;i++){
+        ArrayList<Period> list =new ArrayList<>();
+        for(int i = 0; i< n ;i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            schedul.add(new int[]{start,end});
+            list.add(new Period(start, end));
         }
-        Collections.sort(schedul, (o1,o2)->{
-            int compareInt = Integer.compare(o1[1],o2[1]);
-            if(compareInt == 0){
-                return Integer.compare(o1[0],o2[0]);
-            }
-            return compareInt;
-        }); 
-        // end 값을 기준으로 정렬, 둘의 값이 같다면 start 값을 기준으로 정렬
-        // Integer.compare
-        // 왼쪽값 < 오른쪽값 -1, 왼쪽값 == 오른쪽값 0, 왼쪽값 < 오른쪽값 1
-
-        int cnt = 0;
-        int prev = 0;
-        for(int i = 0;i<schedul.size();i++){
-            if(schedul.get(i)[0] >= prev){
-                cnt++;
-                prev = schedul.get(i)[1];
+        Collections.sort(list,(o1,o2)->{
+            if(o1.end == o2.end) return Integer.compare(o1.start,o2.start);
+            else return Integer.compare(o1.end,o2.end);
+        });
+        int beforeEndtime = 0;
+        int ans = 0;
+        for (Period period : list) {
+            if(beforeEndtime <= period.start){
+                ans++;
+                beforeEndtime = period.end;
             }
         }
-        System.out.println(cnt);
+        System.out.println(ans);
     }
 }
