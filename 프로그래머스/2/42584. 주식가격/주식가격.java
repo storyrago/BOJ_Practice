da267b19a6027d22020f7ef5b038fd270d424b57
Jamin Cheon <40034379+storyrago@users.noreply.github.com>
2026-07-31 14:59:45 +0900

    [level 2] Title: 주식가격, Time: 20.01 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,14 +1,21 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int len = prices.length;
         int[] answer = new int[len];
-        
-        for(int i = 0;i<len;i++){
-            int dura_time = 0;
-            for(int j = i + 1;j<len;j++){
-                answer[i]++;
-                if(prices[j] < prices[i]) break;
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for(int i = 1;i<len;i++){
+            while(!st.isEmpty() && prices[st.peek()] > prices[i]){
+                int index = st.pop();
+                answer[index] = i - index;  
             }
+            st.push(i);
+        }
+        
+        while(!st.isEmpty()){
+            int remainIndex = st.pop();
+            answer[remainIndex] = len - 1 - remainIndex;
         }
         return answer;
     }
